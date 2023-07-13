61851e54d47fb5207e4d3c4ed2106fa7872d6e66
Tyshkan4ik <001vat@gmail.com>
2023-07-13 15:59:56 +0300

    start project

---
+++ Test tasks from Docusketch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test tasks from Docusketch
+//
+//  Created by Виталий Троицкий on 13.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
